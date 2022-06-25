9bf26d826ac427fa925c35abf0dca0a218ff45f8
login <mus666@yandex.ru>
2022-06-25 18:32:58 +1000

    Home_Work1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа на входе принимает 2 числаи выдает, какое число большее, какое число меньшее.
+
+Console.WriteLine("Введите первое число:");
+int number1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 =Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+      Console.WriteLine("Максимальное число"+ number1);
+      Console.WriteLine("Минимальное число"+ number2);      
+}
+else
+{
+      Console.WriteLine("Максимальное число"+ number2);
+      Console.WriteLine("Минимальное число"+ number1);       
+}
